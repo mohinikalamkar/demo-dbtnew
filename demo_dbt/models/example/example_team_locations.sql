29d2942e26e6fd8c3428394db66694e0ef062c82
MKalamkar <mkalamkar@AdminisatorsMBP.attlocal.net>
2022-04-13 12:29:55 -0500

    Use macro from package installed

--- demo_dbt/models/example/example_team_locations.sql
+++ demo_dbt/models/example/example_team_locations.sql
@@ -2,4 +2,5 @@ with final as (
     select * from {{ ref('teams_locations')}}
 )
 
-select * from final
+-- calling function from another packakge
+select name, city, state, {{ dbt_utils.length('city') }} as city_length from final
